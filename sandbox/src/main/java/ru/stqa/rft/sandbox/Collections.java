445eaba4ce2729efffd9b1291a4a89d3816d36e9
T1GROUP\RomanovaD <d.romanova@t1-group.ru>
2017-06-05 22:59:09 +0300

    Добавлены проверки на изменение кол-ва элементов в коллекции Group

---
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Collections.java
@@ -0,0 +1,16 @@
+package ru.stqa.rft.sandbox;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by RomanovaD on 05.06.2017.
+ */
+public class Collections {
+    public static void main(String[] args) {
+        String [] langs = {"Java","C#","Python","PHP"};
+        List<String> languages = Arrays.asList("Java","C#","Python","PHP");
+        for (String l : languages){
+            System.out.println("Я хочу выучить " + l);
+        }
+    }
+}
